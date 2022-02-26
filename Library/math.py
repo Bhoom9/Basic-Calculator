5a128becc04e7cfa6fe74f302d137c15a4a96b09
Bhoom <bhulakshmi.kunchala@Coyotes.usd.edu>
2022-02-26 15:28:13 -0600

    calculator is done

---
+++ Library/math.py
@@ -0,0 +1,11 @@
+def sum( parameter1 , parameter2 ):
+    return parameter1 + parameter2
+
+def sub( parameter1,parameter2):
+    return parameter1 - parameter2
+
+def mul( parameter1 , parameter2):
+    return parameter1 * parameter2
+
+def division( parameter1 , parameter2):
+    return parameter1 / parameter2
